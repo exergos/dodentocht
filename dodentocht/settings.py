e4b34391b03a3305d3cd8158678fb3e8a5059ec7
exergos <willem.lenaerts@exergos.be>
2015-01-29 16:09:09 +0100

    .env file

--- dodentocht/settings.py
+++ dodentocht/settings.py
@@ -142,9 +142,9 @@ STATICFILES_FINDERS = (
 # In production
 #Storage on S3 settings are stored as os.environs to keep settings.py clean
 # if not DEBUG:
-AWS_STORAGE_BUCKET_NAME = 'dodentocht-static'
-AWS_ACCESS_KEY_ID = 'AKIAJSATTDIHX4AQHCGQ'
-AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
 STATIC_URL = S3_URL
