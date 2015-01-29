5c194a825fd96f58d1a9fc0367af2bce5a02cc16
exergos <willem.lenaerts@exergos.be>
2015-01-29 14:01:00 +0100

    Static file fix

--- dodentocht/settings.py
+++ dodentocht/settings.py
@@ -134,3 +134,15 @@ STATICFILES_FINDERS = (
     # other finders..
     'compressor.finders.CompressorFinder',
 )
+
+# ADDED FOR HEROKU DEPLOYMENT
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
