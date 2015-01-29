29b388bd890c8aaa05727cee7f1918ff9d009daf
exergos <willem.lenaerts@exergos.be>
2015-01-29 13:21:27 +0100

    Changes

--- dodentocht/settings.py
+++ dodentocht/settings.py
@@ -77,19 +77,20 @@ DATABASES = {
 
 # CLEARDB_DATABASE_URL = "mysql://b21ac369402104:71a611ed@us-cdbr-iron-east-01.cleardb.net/heroku_a8c4b0cefde9e4f"
 #
-DATABASES = {
-    'default': {
-        'NAME': 'heroku_a8c4b0cefde9e4f',
-        'ENGINE': 'mysql.connector.django',
-        'USER': 'b21ac369402104',
-        'PASSWORD': '71a611ed',
-        'HOST': 'us-cdbr-iron-east-01.cleardb.net',
-        # 'PORT' : '3306',
-        'OPTIONS': {
-          'autocommit': True,
-        },
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'NAME': 'heroku_a8c4b0cefde9e4f',
+#         'ENGINE': 'mysql.connector.django',
+#         'USER': 'b21ac369402104',
+#         'PASSWORD': '71a611ed',
+#         'HOST': 'us-cdbr-iron-east-01.cleardb.net',
+#         # 'PORT' : '3306',
+#         'OPTIONS': {
+#           'autocommit': True,
+#           'connect_timeout': 20000,
+#         },
+#     }
+# }
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
