9bd811666c7a39994fa293c02857c650dfc5c2a4
exergos <willem.lenaerts@exergos.be>
2015-01-29 17:49:26 +0100

    Create dummy humans.txt in static map

--- dodentocht/settings.py
+++ dodentocht/settings.py
@@ -52,11 +52,11 @@ else:
     # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = 'static'
+    STATIC_ROOT = 'staticfiles'
     STATIC_URL = '/static/'
 
     STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'staticfiles'),
+        os.path.join(BASE_DIR, 'static'),
     )
     # Simplified static file serving.
     # https://warehouse.python.org/project/whitenoise/
