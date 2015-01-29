9b6a2f606a7a1c51123d2456a8a25d649d99a99c
exergos <willem.lenaerts@exergos.be>
2015-01-29 16:00:07 +0100

    AWS no os.environ variables in settings.py

--- dodentocht/settings.py
+++ dodentocht/settings.py
@@ -142,9 +142,9 @@ STATICFILES_FINDERS = (
 # In production
 #Storage on S3 settings are stored as os.environs to keep settings.py clean
 # if not DEBUG:
-AWS_STORAGE_BUCKET_NAME = os.environ['dodentocht-static']
-AWS_ACCESS_KEY_ID = os.environ['AKIAJSATTDIHX4AQHCGQ']
-AWS_SECRET_ACCESS_KEY = os.environ['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']
+AWS_STORAGE_BUCKET_NAME = 'dodentocht-static'
+AWS_ACCESS_KEY_ID = 'AKIAJSATTDIHX4AQHCGQ'
+AWS_SECRET_ACCESS_KEY = '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'
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
 STATIC_URL = S3_URL
